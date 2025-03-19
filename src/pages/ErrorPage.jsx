323feb24bd29d0941848fdf78692259c9ef88e1f
Adrian Rogers <adritek@gmail.com>
2025-03-20 00:26:37 +0100

    Organise file names

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router';
+
+export default function Error() {
+  return (
+    <div className="text-center w-2xl mx-auto mt-4">
+      <h1 className="text-7xl">404</h1>
+      <h2 className="text-6xl">Page not found, click lil-dude to go back home</h2>
+      <p id="lil-dude" className="text-5xl">
+        <Link to="/" className="text-9xl underline">
+          🤷‍♂️
+        </Link>
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">How it was built</h1>
+      <p>So basically - from the README, there was 2 pages that needed to built</p>
+      <ul className="list-disc list-inside">
+        <li>this one - describing how I built this SPA</li>
+        <li>and a table with output data</li>
+      </ul>
+      <p>I went with:</p>
+      <ul className="list-disc list-inside">
+        <li>React and Vite - it is the defaco now-a-days</li>
+        <li>
+          React Router DOM - to build out the content and error pages (usage is v6)
+        </li>
+        <li>React Query - it will grab the data (cache it, or refetch bts)</li>
+        <li>Tailwind CSS - because it&#39;s the flavour of the month</li>
+      </ul>
+    </>
+  );
+}
