6736c8fc9a75a78f3d7fb06521bfe8ba8f8e7bea
Adrian Rogers <adritek@gmail.com>
2025-04-25 15:26:57 +0200

    feat: rewrite with hashrouter and deploy workflow
    - Added deploy workflow
    - Updated package.json
    - Switched to hashrouter for GitHub Pages
    - Moved data.json to public folder
    - Tidy up CSS
    
    add deploy workflow
    
    update package.json
    
    trailing slash and urls
    
    Save point, gh pages uses hashrouter fyi
    
    save point for rewrite with hashrouter for gh pages
    
    Hash router is configured and working, need to fix 'orders'
    
    added hasrouter, moved data.json to public folder, tidy up some CSS
    
    Trying to make a one-liner out of the table instead of line wrapping
    
    save point - working

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { HashRouter, Route, Routes } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from './App.jsx';
@@ -15,15 +15,15 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<HomePage />} />
             <Route path="orders" element={<OrdersPage />} />
+            <Route path="*" element={<ErrorPage />} />
           </Route>
-          <Route path="*" element={<ErrorPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </StrictMode>
 );
