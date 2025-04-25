4088ae405fceb2ac1c0a4f1d178f7346ae915a78
Adrian Rogers <adritek@gmail.com>
2025-04-25 15:23:17 +0200

    re-added base into vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/kantox-table',
+  base: '/kantox-table/',
 });
